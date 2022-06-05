346a5f92dafeb587f99fbba25ef5048a565bdc3f
mehdi <mehdiprogrammer30@gmail.com>
2022-06-05 19:02:03 +0430

    final Email

---
+++ app/Http/Controllers/Admin/Notify/EmailController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Notify;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Notify\EmailRequest;
+use App\Models\Notify\Email;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $emails = Email::orderBy('created_at', 'desc')->simplePaginate(15);
+        return view('admin.notify.email.index', compact('emails'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.notify.email.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(EmailRequest $request)
+    {
+        $inputs = $request->all();
+        //date fixed
+        $realTimestampStart = substr($request->published_at, 0, 10);
+        $inputs['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampStart);
+        $email = Email::create($inputs);
+        return redirect()->route('admin.notify.email.index')->with('swal-success', 'ایمیل شما با موفقیت ثبت شد');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Email $email)
+    {
+        return view('admin.notify.email.edit', compact('email'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(EmailRequest $request, Email $email)
+    {
+        $inputs = $request->all();
+        //date fixed
+        $realTimestampStart = substr($request->published_at, 0, 10);
+        $inputs['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampStart);
+        $email->update($inputs);
+        return redirect()->route('admin.notify.email.index')->with('swal-success', 'ایمیل شما با موفقیت ویرایش شد');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Email $email)
+    {
+        $result = $email->delete();
+        return redirect()->route('admin.notify.email.index')->with('swal-success', 'ایمیل شما با موفقیت حذف شد');
+    }
+
+
+    public function status(Email $email){
+
+        $email->status = $email->status == 0 ? 1 : 0;
+        $result = $email->save();
+        if($result){
+            if($email->status == 0){
+                return response()->json(['status' => true, 'checked' => false]);
+            }
+            else{
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        }
+        else{
+            return response()->json(['status' => false]);
+        }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\Admin\Content\CategoryController;
 use App\Http\Controllers\Admin\Content\MenuController;
 use App\Http\Controllers\Admin\Content\PageController;
 use App\Http\Controllers\Admin\Content\PostController;
+use App\Http\Controllers\Admin\Notify\EmailController;
+use App\Http\Controllers\Admin\Notify\SMSController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -257,29 +259,33 @@ Route::prefix('admin')->namespace('Admin')->group(function (){
 //
 //    });
 
-    //    Route::prefix('notify')->namespace('Notify')->group(function(){
-    //
-    //        //email
-    //        Route::prefix('email')->group(function(){
-    //            Route::get('/', [EmailController::class, 'index'])->name('admin.notify.email.index');
-    //            Route::get('/create', [EmailController::class, 'create'])->name('admin.notify.email.create');
-    //            Route::post('/store', [EmailController::class, 'store'])->name('admin.notify.email.store');
-    //            Route::get('/edit/{id}', [EmailController::class, 'edit'])->name('admin.notify.email.edit');
-    //            Route::put('/update/{id}', [EmailController::class, 'update'])->name('admin.notify.email.update');
-    //            Route::delete('/destroy/{id}', [EmailController::class, 'destroy'])->name('admin.notify.email.destroy');
-    //        });
-    //
-    //        //sms
-    //        Route::prefix('sms')->group(function(){
-    //            Route::get('/', [SMSController::class, 'index'])->name('admin.notify.sms.index');
-    //            Route::get('/create', [SMSController::class, 'create'])->name('admin.notify.sms.create');
-    //            Route::post('/store', [SMSController::class, 'store'])->name('admin.notify.sms.store');
-    //            Route::get('/edit/{id}', [SMSController::class, 'edit'])->name('admin.notify.sms.edit');
-    //            Route::put('/update/{id}', [SMSController::class, 'update'])->name('admin.notify.sms.update');
-    //            Route::delete('/destroy/{id}', [SMSController::class, 'destroy'])->name('admin.notify.sms.destroy');
-    //        });
-    //
-    //    });
+        Route::prefix('notify')->namespace('Notify')->group(function(){
+
+            //email
+            Route::prefix('email')->group(function(){
+                Route::get('/', [EmailController::class, 'index'])->name('admin.notify.email.index');
+                Route::get('/create', [EmailController::class, 'create'])->name('admin.notify.email.create');
+                Route::post('/store', [EmailController::class, 'store'])->name('admin.notify.email.store');
+                Route::get('/edit/{email}', [EmailController::class, 'edit'])->name('admin.notify.email.edit');
+                Route::put('/update/{email}', [EmailController::class, 'update'])->name('admin.notify.email.update');
+                Route::delete('/destroy/{email}', [EmailController::class, 'destroy'])->name('admin.notify.email.destroy');
+                Route::get('/status/{email}', [EmailController::class, 'status'])->name('admin.notify.email.status');
+
+            });
+
+            //sms
+            Route::prefix('sms')->group(function(){
+                Route::get('/', [SMSController::class, 'index'])->name('admin.notify.sms.index');
+                Route::get('/create', [SMSController::class, 'create'])->name('admin.notify.sms.create');
+                Route::post('/store', [SMSController::class, 'store'])->name('admin.notify.sms.store');
+                Route::get('/edit/{sms}', [SMSController::class, 'edit'])->name('admin.notify.sms.edit');
+                Route::put('/update/{sms}', [SMSController::class, 'update'])->name('admin.notify.sms.update');
+                Route::delete('/destroy/{sms}', [SMSController::class, 'destroy'])->name('admin.notify.sms.destroy');
+                Route::get('/status/{sms}', [SMSController::class, 'status'])->name('admin.notify.sms.status');
+
+            });
+
+        });
 
     //    Route::prefix('ticket')->namespace('Ticket')->group(function(){
     //
