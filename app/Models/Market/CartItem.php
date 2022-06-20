55008967a67d7183dba589e0329f2c6b7a466251
mehdi <mehdiprogrammer30@gmail.com>
2022-06-20 16:32:04 +0430

    show Cart part 2

--- app/Models/Market/CartItem.php
+++ app/Models/Market/CartItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Market;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +12,63 @@ class CartItem extends Model
     use HasFactory,SoftDeletes;
 
     protected $fillable=['user_id','product_id','color_id','guarantee_id','number'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function guarantee()
+    {
+        return $this->belongsTo(Guarantee::class);
+    }
+
+
+    public function color()
+    {
+        return $this->belongsTo(ProductColor::class);
+    }
+
+
+
+    //productPrice + colorPrice + guranateePrice
+    public function cartItemProductPrice()
+    {
+        $guaranteePriceIncrease = empty($this->guarantee_id) ? 0 : $this->guarantee->price_increase;
+        $colorPriceIncrease = empty($this->color_id) ? 0 : $this->color->price_increase;
+        return $this->product->price + $guaranteePriceIncrease + $colorPriceIncrease;
+    }
+
+
+    // productPrice * (discountPerecentage / 100)
+    public function cartItemProductDiscount()
+    {
+        $cartItemProductPrice = $this->cartItemProductPrice();
+        $productDiscount = empty($this->product->activeAmazingSale()) ? 0 : $cartItemProductPrice * ($this->product->activeAmazingSale()->percentage / 100);
+        return $productDiscount;
+    }
+
+
+    //number * (productPrice + colorPrice + guranateePrice - discountPrice)
+    public function cartItemFinalPrice()
+    {
+        $cartItemProductPrice = $this->cartItemProductPrice();
+        $productDiscount = $this->cartItemProductDiscount();
+        return $this->number * ($cartItemProductPrice - $productDiscount);
+    }
+
+
+    //number * productDiscount
+    public function cartItemFinalDiscount()
+    {
+        $productDiscount = $this->cartItemProductDiscount();
+        return $this->number * $productDiscount;
+    }
 }
