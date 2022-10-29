ae8cd55f0b4876c12c1e65adbba2c408cac21c9c
mehdi <mehdiprogrammer30@gmail.com>
2022-10-29 15:04:18 +0330

    Pardakht Offline

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 
+
     <!-- start slideshow -->
     <section class="container-xxl my-4">
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{session('success')}}
+            </div>
+        @endif
         <section class="row">
             <section class="col-md-8 pe-md-1 ">
                 <section id="slideshow" class="owl-carousel owl-theme">
