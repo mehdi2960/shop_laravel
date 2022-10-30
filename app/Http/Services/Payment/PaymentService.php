45756e781c2157219d63737a5ac109553e52767f
mehdi <mehdiprogrammer30@gmail.com>
2022-10-30 12:33:13 +0330

    Taied Pardakht dar Zarinpal

--- app/Http/Services/Payment/PaymentService.php
+++ app/Http/Services/Payment/PaymentService.php
@@ -9,7 +9,17 @@ use Zarinpal\Clients\GuzzleClient;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Client\RequestException;
 
-class PaymentService{
+class PaymentService
+{
+
+    public function paymentCallback()
+    {
+        $amount = 0;
+        $result = $this->zarinpalVerify($amount);
+        if ($result['success']) {
+            return 'ok';
+        }
+    }
 
     public function zarinpal()
     {
@@ -26,25 +36,56 @@ class PaymentService{
             'amount' => $amount,
             'description' => 'the order',
         ];
-        try{
+        try {
             $response = $zarinpal->request($payment);
             $onlinePayment = OnlinePayment::first();
             $code = $response['data']['code'];
             $message = $zarinpal->getCodeMessage($code);
-            if($code === 100)
-            {
+            if ($code === 100) {
                 $onlinePayment->update(['bank_first_response' => ($response)]);
                 $authority = $response['data']['authority'];
                 return $zarinpal->redirect($authority);
             }
 
-        }
-        catch(RequestException $exception){
+        } catch (RequestException $exception) {
 
             return false;
 
         }
     }
+
+    public function zarinpalVerify($amount)
+    {
+        $authority = $_GET['Authority'];
+        $data = ['merchent_id' => Config::get('payment.zarinpal_api_key'), 'authority' => $authority, 'amount' => (int)$amount];
+        $jsonData = json_encode($data);
+        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Zarinpal Rest Api v4');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type:application/json',
+            'Content-Length:' . strlen($jsonData)
+        ));
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($result, true);
+        $onlinePayment = OnlinePayment::first();
+        $onlinePayment->update(['bank_second_response' => $result]);
+        if (count($result['errors']) === 0) {
+            if ($result['data']['code'] == 100) {
+                return ['success' => true];
+            } else {
+                return ['success' => false];
+            }
+        } else {
+            return ['success' => false];
+        }
+
+    }
+
+    
 
 }
 
