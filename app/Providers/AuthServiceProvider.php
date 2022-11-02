f50b4fc44819f78f4efb4dad1b1e07f348ce2d67
mehdi <mehdiprogrammer30@gmail.com>
2022-11-02 11:57:51 +0330

    Policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Content\Post;
 use App\Models\User;
+use App\Policies\PostPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -16,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Post::class => PostPolicy::class
     ];
 
     /**
@@ -27,9 +29,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('update-post', function (User $user, Post $post) {
-            return $user->id === $post->author_id;
-        });
+//        Gate::define('update-post', function (User $user, Post $post) {
+//            return $user->id === $post->author_id;
+//        });
+
+        Gate::define('update-post', [PostPolicy::class, 'update']);
 
 //        Gate::define('update-post', function(User $user) {
 //            //     return $user->user_type === 1 ? Response::allow()
